c3228c6652c28d5a6610c87098ffd14d9da72458
ruslan-kornich <korny4rus143@gmail.com>
2023-03-06 18:20:00 +0200

    simple employee tree

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from app.models import Employee
+
+
 # Create your views here.
+
+
+def employee_tree(request):
+    employees = Employee.objects.all()
+    count_all = Employee.objects.count()
+    data = {"employees": employees, "count_all": count_all}
+    return render(request, template_name="app/employee_tree.html", context=data)
