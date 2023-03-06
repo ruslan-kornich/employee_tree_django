82febe51e4fc5feef064dd5f177ab04b6808b832
ruslan-kornich <korny4rus143@gmail.com>
2023-03-06 16:32:08 +0200

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Employee(MPTTModel):
+    class MPTTMeta:
+        order_insertion_by = ["full_name"]  # By which field to sort children's in tree
+
+    full_name = models.CharField(max_length=255, db_index=True)
+    position = models.CharField(max_length=255, db_index=True)
+    hire_date = models.DateField()
+    email = models.EmailField(max_length=254)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+
+    def __str__(self):
+        return self.full_name
