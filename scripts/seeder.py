0cd8256fa18acd4be2f42099998bca5036638569
ruslan-kornich <korny4rus143@gmail.com>
2023-03-06 20:36:29 +0200

    seeder

--- scripts/seeder.py
+++ scripts/seeder.py
@@ -1,3 +1,4 @@
+import random
 from django_seed import Seed
 
 from app.models import Employee
@@ -8,17 +9,35 @@ def first_record():
         full_name="Зінченко Лада Левівна",
         position="Генеральний директор",
         hire_date="2010-02-16",
-        email="director@i.ua")
+        email="director@i.ua",
+    )
     employee.save()
 
 
-def fill_up_data_base():
-    seeder = Seed.seeder(locale='uk_UA')
-    seeder.add_entity(Employee, 10, {
-        'full_name': lambda x: seeder.faker.name(),
-        'position': lambda x: seeder.faker.bs(),
-        'hire_date': lambda x: seeder.faker.date(),
-        'email': lambda x: seeder.faker.email(),
-
-    })
+def fill_up_data_base(count, position, year, parent_min, parent_max):
+    seeder = Seed.seeder(locale="uk_UA")
+    seeder.add_entity(
+        Employee,
+        count,
+        {
+            "full_name": lambda x: seeder.faker.name(),
+            "position": position,
+            "hire_date": f"{year}-{random.randint(1, 12)}-{random.randint(1, 28)}",
+            "email": lambda x: seeder.faker.email(),
+            "parent": lambda x: Employee.objects.get(
+                id=random.randint(parent_min, parent_max)
+            ),
+        },
+    )
     seeder.execute()
+
+
+def run():
+    first_record()
+    fill_up_data_base(10, "Регіональний директор", 2010, 1, 1)
+    fill_up_data_base(100, "Директор по збуту продукції", 2011, 2, 11)
+    fill_up_data_base(1000, "Менеджер по закупівлях", 2012, 112, 1111)
+    fill_up_data_base(1000, "Комплектувальник", 2012, 1112, 2112)
+    fill_up_data_base(30000, "Менеджер з продажу", 2014, 2, 11)
+    fill_up_data_base(10000, "Водій", 2019, 112, 1111)
+    fill_up_data_base(10000, "Стажер", 2022, 1112, 2112)
